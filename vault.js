ac968c5edd0748981920aa063b7f343a289e1d04
Harsh Kotak <kotak_harsh@hotmail.com>
2018-07-07 17:06:01 -1000

    completed exercise

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
-  
+
+    
+    const myObj = {};
+
+    const setValue = function(key,value) {
+        myObj[key] = value;
+    };
+
+    const getValue = function(key) {
+        if (key in myObj) {
+            return myObj[key];
+        } else {
+            return null;
+        }
+    }
+
+    return {
+        setValue: setValue,
+        getValue: getValue
+    }
+
+    
+
 };
